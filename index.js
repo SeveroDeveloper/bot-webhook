d06e0c03f7a7292eaddb20912e9f561c00f01f48
Amanda Severo <amanda.severo@take.net>
2021-07-22 20:41:07 -0300

    heroku validation

--- index.js
+++ index.js
@@ -3,6 +3,10 @@ const express = require('express');
 const app = express();
 const port = 3000;
 
+app.get('/', (req, res) => {
+    return res.json({"works":"true"});
+});
+
 app.post('/', (req, res) => {
     console.log(req);
     res.status(201).send('ok');
