1acb2a18dd973232d39bfc99783e0acab938838f
Amanda Severo <amanda.severo@take.net>
2021-07-22 22:34:30 -0300

    all done, displaying results

--- index.js
+++ index.js
@@ -1,17 +1,24 @@
 const express = require('express');
+const bodyParser = require('body-parser');
 require('dotenv').config(); 
 
 const app = express();
 const port = process.env.PORT || 3000;
+app.use(bodyParser.json());
 
-app.get('/', (req, res) => {
-    return res.json({"works":"true"});
-});
+var data;
 
 app.post('/', (req, res) => {
-    console.log(req);
-    res.status(201).send('ok');
+    console.log(req.body);
+    data = req.body;
+    res.status(200).send('ok');
 });
+
+app.get('/', (req, res) => {
+    return res.json(data);
+});
+
+
 
 app.listen(port, () => {console.log(`Running: ${port}`)});
 
