3d40267b81afb96e62346d8889e5e1371e7b2ab3
Amanda Severo <amanda.severo@take.net>
2021-07-22 21:12:01 -0300

    trying to fix heroku

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const express = require('express');
 const app = express();
 const port = 3000;
 
-app.get('/start', (req, res) => {
+app.get('/', (req, res) => {
     return res.json({"works":"true"});
 });
 
